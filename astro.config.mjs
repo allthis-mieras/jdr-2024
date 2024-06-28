4e4b77156866fd8216244b9ac9e30f76e9604a6c
mieras <mieras@gmail.com>
2024-06-28 11:37:28 +0200

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	site: 'https://jeffreyderoode.nl/',
-	output: 'server',
-	adapter: netlify({
-		imageCDN: false,
-	}),
 });
